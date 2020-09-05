cdf7dfa8ef8dd17138d3200ca6cbc84ff11359ec
arondeep <70179716+arondeep@users.noreply.github.com>
2020-09-04 21:20:22 -0500

    landing-oficial
    oficial version

---
+++ js/main.js
@@ -0,0 +1,25 @@
+var scroll = new SmoothScroll('a[href*="#section2"]', {
+    offset: 400,
+    speed: 800
+});
+var scroll = new SmoothScroll('a[href*="#section5"]', {
+    offset: 150,
+    speed: 800
+});
+var scroll = new SmoothScroll('a[href*="#section7"]', {
+    offset: 150,
+    speed: 600
+});
+var scroll = new SmoothScroll('a[href*="#section9"]', {
+    offset: 50,
+    speed: 400
+});
+var scroll = new SmoothScroll('a[href*="#section11"]', {
+    offset: 150,
+    speed: 200
+});
+var scroll = new SmoothScroll('a[href*="#section3"]', {
+    offset: 50,
+    speed: 600
+});
+AOS.init();
